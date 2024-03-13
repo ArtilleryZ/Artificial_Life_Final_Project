8741978350cf0da6927355c64aebf1540745162d
ArtilleryZ <x33881320@gmail.com>
2024-03-13 18:05:52 -0500

    done with frame

--- main_script.py
+++ main_script.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 import mujoco
 import mujoco_viewer
+import copy
 import time
 from mujoco import viewer
 import dm_control.mujoco as dm
@@ -10,6 +11,16 @@ from xml_generator import generate_robot_xml
 
 from helper_function import data_initialization
 from helper_function import parameter_boundary
+
+
+max_iterations = 5
+pos_lr = 0.05
+size_lr = 0.1
+learning_rate = 0.05
+input_dim = 20
+hidden_dim = 200
+output_dim = 20
+lr_NN = 0.005
 
 
 
@@ -33,8 +44,27 @@ class four_layer_NN(nn.Module):
         x = self.output_layer(x)
         return x
 
-def apply_adjustment(parameter, adjustment, learning_rate):
-    new = parameter + learning_rate * adjustment
+
+
+
+def apply_adjustment(parameter, adjustment, pos_lr, size_lr, update_ratio=0.5):
+    num_params = len(parameter)
+    num_updates = int(update_ratio * num_params)
+    update_indices = np.random.choice(num_params, num_updates, replace=False)
+    
+    pos_indices = [0, 1, 5, 6, 10, 11, 15, 16]
+    size_indices = set(range(num_params)) - set(pos_indices)
+    
+    new = parameter.copy()
+    
+    for i in update_indices:
+        if i in pos_indices:
+            lr = pos_lr
+        else:
+            lr = size_lr
+        
+        new[i] = parameter[i] + lr * adjustment[i]
+
     new = np.clip(new, a_min=lower_bounds, a_max=upper_bounds)
     return new
 
@@ -81,7 +111,7 @@ def simulate_robot(num_iter, x_size, y_size, z_size, parameter):
         d.ctrl[j] = 5
     time.sleep(1)
     
-    for i in range(1000):
+    for i in range(1200):
         
         height.append(d.sensordata[2])
         
@@ -108,25 +138,21 @@ def simulate_robot(num_iter, x_size, y_size, z_size, parameter):
     
     max_height = [max(height)]
     
+    if max_height[0] >= 4:
+        return -1
+    
     combined_data = [robot_geometry + max_height]
     
-    output_filename = "data/maxheight"
+    raw_filename = "data/rawmaxheight"
     
-    output_filename = output_filename + "_x_" + str(round(x_size,3)) + "_y_" + str(round(y_size,3)) + "_z_" + str(round(z_size,3)) + ".csv"
+    raw_filename = raw_filename + "_x_" + str(round(x_size,3)) + "_y_" + str(round(y_size,3)) + "_z_" + str(round(z_size,3)) + ".csv"
     
     
-    with open(output_filename, 'ab') as f:
+    with open(raw_filename, 'ab') as f:
         np.savetxt(f, combined_data, delimiter=',')
         
     return max_height
 
-
-max_iterations = 500
-learning_rate = 0.05
-input_dim = 20
-hidden_dim = 100
-output_dim = 20
-lr_NN = 0.005
 
 nn_model = four_layer_NN(input_dim, hidden_dim, output_dim)
 
@@ -134,24 +160,52 @@ robot_init = data_initialization()
 x_size, y_size, z_size = robot_init[:3]
 parameter = robot_init[3:]
 
+best_parameter = copy.deepcopy(parameter)
+best_height = -np.inf 
+
 lower_bounds, upper_bounds = parameter_boundary()
 
-optimizer = torch.optim.Adam(nn_model.parameter(), lr_NN)
+optimizer = torch.optim.Adam(nn_model.parameters(), lr_NN)
 
 
 for itr in range(max_iterations):
     parameter_tensor = torch.tensor(parameter, dtype=torch.float32).unsqueeze(0)
     adjustment = nn_model(parameter_tensor).squeeze().detach().numpy()
-    parameter = apply_adjustment(parameter, adjustment, learning_rate)
+    new = apply_adjustment(parameter, adjustment, pos_lr, size_lr, 0.2)
+    
 
-    max_height = simulate_robot(itr, x_size, y_size, z_size, parameter)
-    if max_height is not None:
-        loss = -torch.tensor(max_height, dtype=torch.float, requires_grad=True)
+    max_height = simulate_robot(itr, x_size, y_size, z_size, new)
+    
+    
+    if max_height[0] == -1:
+        print("Error: simulator broke at this generation.")
+        continue
+    
+    if max_height[0] > best_height:
+        print(f"Generation {itr+1}, Height Improved: {max_height}")
+        best_height = max_height[0]
+        best_parameter = copy.deepcopy(new)
+        parameter = best_parameter
+        
+
+        loss = torch.tensor([max_height[0]], dtype=torch.float, requires_grad=True)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-        print(f"Generation {itr+1}, Loss: {loss.item()}, Max Height: {max_height}")
     else:
-        print("Error: simulator broke at this generation.")
+        parameter = best_parameter
+        print(f"Generation {itr+1}, No Improvement. Reverting to previous best height")
+        
+        
+    final_data = [best_parameter + best_height]
+    
+    main_filename = "data/maxheight"
+
+    
+    main_filename = main_filename + "_x_" + str(round(x_size,3)) + "_y_" + str(round(y_size,3)) + "_z_" + str(round(z_size,3)) + ".csv"
+    
+    with open(main_filename, 'ab') as f:
+        np.savetxt(f, final_data, delimiter=',')
+        
+    
---
+++ run_result.py
@@ -0,0 +1,64 @@
+import mujoco
+import numpy as np
+import mujoco_viewer
+import time
+from mujoco import viewer
+import dm_control.mujoco as dm
+from xml_generator import generate_robot_xml
+
+data = np.loadtxt('data/maxheight_x_0.186_y_0.16_z_0.089.csv', delimiter=',')
+
+height = data[:, 23]
+idx_min_height = np.argmin(height)
+min_height = height[idx_min_height]
+idx_max_height = np.argmax(height)
+max_height = height[idx_max_height]
+
+parameters_min_height = data[idx_min_height, :-1]
+parameters_max_height = data[idx_max_height, :-1]
+
+def run_simulation(parameters, filename_suffix):
+    x_size, y_size, z_size = parameters[:3]
+    leg1_x_pos_l, leg1_z_pos_l, leg1_x_size_l, leg1_y_size_l, leg1_z_size_l = parameters[3:8]
+    leg2_x_pos_l, leg2_z_pos_l, leg2_x_size_l, leg2_y_size_l, leg2_z_size_l = parameters[8:13]
+    leg1_x_pos_r, leg1_z_pos_r, leg1_x_size_r, leg1_y_size_r, leg1_z_size_r = parameters[13:18]
+    leg2_x_pos_r, leg2_z_pos_r, leg2_x_size_r, leg2_y_size_r, leg2_z_size_r = parameters[18:23]
+    
+    filename = "temp/maxheight_{filename_suffix}.xml"
+    generate_robot_xml(filename,
+                       x_size, y_size, z_size,
+                       leg1_x_size_l, leg1_y_size_l, leg1_z_size_l,
+                       leg2_x_size_l, leg2_y_size_l, leg2_z_size_l,
+                       leg1_x_pos_l, leg1_z_pos_l,
+                       leg2_x_pos_l, leg2_z_pos_l,
+                       leg1_x_size_r, leg1_y_size_r, leg1_z_size_r,
+                       leg2_x_size_r, leg2_y_size_r, leg2_z_size_r,
+                       leg1_x_pos_r, leg1_z_pos_r,
+                       leg2_x_pos_r, leg2_z_pos_r)
+
+    m = mujoco.MjModel.from_xml_path(filename)
+    d = mujoco.MjData(m)
+    viewer = mujoco_viewer.MujocoViewer(m, d)
+    
+    for j in range(len(d.ctrl)):
+        d.ctrl[j] = 5
+    time.sleep(1)
+
+    for i in range(3000):
+        if viewer.is_alive:
+            mujoco.mj_step(m, d)
+            viewer.render()
+        else:
+            break
+    
+    viewer.close()
+
+
+
+
+
+run_simulation(parameters_min_height, "min_height")
+print(f"Generation {0}, Height: {min_height}")
+
+run_simulation(parameters_max_height, "max_height")
+print(f"Generation {idx_max_height}, Height: {max_height}")
